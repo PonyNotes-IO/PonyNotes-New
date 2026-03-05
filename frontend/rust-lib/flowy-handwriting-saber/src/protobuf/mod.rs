4ce8176c8f16cdbb3f62e8b260832b4443f790e7
kevin8341 <87103978@qq.com>
2026-03-05 20:42:12 +0800

    删除 protobuf 相关的生成文件，包括 entities.rs、event_map.rs 和 mod.rs，以清理项目结构。

--- frontend/rust-lib/flowy-handwriting-saber/src/protobuf/mod.rs
+++
@@ -1,9 +0,0 @@
-#![cfg_attr(rustfmt, rustfmt::skip)]
- #![allow(ambiguous_glob_reexports)]
-// Auto-generated, do not edit
-
-mod entities;
-pub use entities::*;
-
-mod event_map;
-pub use event_map::*;
