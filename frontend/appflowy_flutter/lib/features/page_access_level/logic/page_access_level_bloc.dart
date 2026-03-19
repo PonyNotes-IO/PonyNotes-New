a17939462663c4ecf970b3b7abbbe8aa191f361f
metacodebean <caokun8008@gmail.com>
2026-03-19 21:09:29 +0800

    特性：添加刷新访问级别事件和共享用户的更新监听器

--- frontend/appflowy_flutter/lib/features/page_access_level/logic/page_access_level_bloc.dart
+++ frontend/appflowy_flutter/lib/features/page_access_level/logic/page_access_level_bloc.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:typed_data';
 
+import 'package:appflowy/core/notification/folder_notification.dart';
 import 'package:appflowy/features/page_access_level/data/repositories/page_access_level_repository.dart';
 import 'package:appflowy/features/page_access_level/data/repositories/rust_page_access_level_repository_impl.dart';
 import 'package:appflowy/features/page_access_level/logic/page_access_level_event.dart';
@@ -7,7 +9,10 @@ import 'package:appflowy/features/page_access_level/logic/page_access_level_stat
 import 'package:appflowy/features/share_tab/data/models/models.dart';
 import 'package:appflowy/shared/feature_flags.dart';
 import 'package:appflowy/workspace/application/view/view_listener.dart';
+import 'package:appflowy_backend/protobuf/flowy-error/errors.pb.dart';
+import 'package:appflowy_backend/protobuf/flowy-folder/notification.pb.dart';
 import 'package:appflowy_backend/protobuf/flowy-folder/view.pb.dart';
+import 'package:appflowy_result/appflowy_result.dart';
 import 'package:bloc/bloc.dart';
 import 'package:protobuf/protobuf.dart';
 
@@ -28,6 +33,7 @@ class PageAccessLevelBloc
     on<PageAccessLevelUnlockEvent>(_onUnlock);
     on<PageAccessLevelUpdateLockStatusEvent>(_onUpdateLockStatus);
     on<PageAccessLevelUpdateSectionTypeEvent>(_onUpdateSectionType);
+    on<PageAccessLevelRefreshAccessLevelEvent>(_onRefreshAccessLevel);
   }
 
   final ViewPB view;
@@ -39,6 +45,9 @@ class PageAccessLevelBloc
   // Used to listen for view updates.
   late final ViewListener listener;
 
+  // Used to listen for shared users update notifications.
+  FolderNotificationListener? _sharedUsersListener;
+
   // should ignore the page access level
   // in the row details page, we don't need to check the page access level
   final bool ignorePageAccessLevel;
@@ -46,6 +55,7 @@ class PageAccessLevelBloc
   @override
   Future<void> close() async {
     await listener.stop();
+    await _sharedUsersListener?.stop();
     return super.close();
   }
 
@@ -57,6 +67,17 @@ class PageAccessLevelBloc
     listener.start(
       onViewUpdated: (view) async {
         add(PageAccessLevelEvent.updateLockStatus(view.isLocked));
+      },
+    );
+
+    // Listen for shared users updates so we can re-check access level
+    // when the cloud fetches the latest permissions from the backend.
+    _sharedUsersListener = FolderNotificationListener(
+      objectId: view.id,
+      handler: (FolderNotification ty, FlowyResult<Uint8List, FlowyError> _) {
+        if (ty == FolderNotification.DidUpdateSharedUsers) {
+          add(const PageAccessLevelEvent.refreshAccessLevel());
+        }
       },
     );
 
@@ -157,6 +178,25 @@ class PageAccessLevelBloc
     emit(
       state.copyWith(
         sectionType: event.sectionType,
+      ),
+    );
+  }
+
+  Future<void> _onRefreshAccessLevel(
+    PageAccessLevelRefreshAccessLevelEvent event,
+    Emitter<PageAccessLevelState> emit,
+  ) async {
+    if (!FeatureFlag.sharedSection.isOn || ignorePageAccessLevel) {
+      return;
+    }
+
+    final accessLevel = await repository.getAccessLevel(view.id);
+    emit(
+      state.copyWith(
+        accessLevel: accessLevel.fold(
+          (accessLevel) => accessLevel,
+          (_) => ShareAccessLevel.readOnly,
+        ),
       ),
     );
   }
