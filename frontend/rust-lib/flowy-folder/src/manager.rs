b29ce025b5fa5fadd118ddaa47fa3e65c2034c90
metacodebean <caokun8008@gamil.com>
2026-06-17 09:45:06 +0800

    fix: 在 FolderManager 中初始化 user_id 为 None

--- frontend/rust-lib/flowy-folder/src/manager.rs
+++ frontend/rust-lib/flowy-folder/src/manager.rs
@@ -1683,6 +1683,7 @@ impl FolderManager {
           },
           pending_invitation: false,
           // 本地数据库表 WorkspaceSharedUserTable 未存储 user_id，从本地缓存重建时填 None
+          user_id: None,
         })
         .collect();
 
