df081fa50812901a8833943392009198d6376af3
hudi0705 <1830225455@qq.com>
2026-07-24 14:50:13 +0800

    feat：修改设置存储路径无效

--- frontend/appflowy_flutter/lib/features/settings/data/repositories/rust_settings_repository_impl.dart
+++ frontend/appflowy_flutter/lib/features/settings/data/repositories/rust_settings_repository_impl.dart
@@ -1,6 +1,5 @@
 import 'package:appflowy/startup/startup.dart';
 import 'package:appflowy/startup/tasks/rust_sdk.dart';
-import 'package:appflowy/user/application/user_settings_service.dart';
 import 'package:appflowy/workspace/application/settings/prelude.dart';
 import 'package:appflowy_backend/protobuf/flowy-error/protobuf.dart';
 import 'package:appflowy_result/appflowy_result.dart';
@@ -11,22 +10,17 @@ import 'settings_repository.dart';
 class RustSettingsRepositoryImpl implements SettingsRepository {
   const RustSettingsRepositoryImpl();
 
-  final _userBackendService = const UserSettingsBackendService();
-
   @override
   Future<FlowyResult<UserDataLocation, FlowyError>>
       getUserDataLocation() async {
     final defaultDirectory = (await appFlowyApplicationDataDirectory()).path;
-    final result = await _userBackendService.getUserSetting();
+    final configuredDirectory = await getIt<ApplicationDataStorage>().getPath();
 
-    return result.map(
-      (settings) {
-        final userDirectory = settings.userFolder;
-        return UserDataLocation(
-          path: userDirectory,
-          isCustom: userDirectory.contains(defaultDirectory),
-        );
-      },
+    return FlowyResult.success(
+      UserDataLocation(
+        path: configuredDirectory,
+        isCustom: !configuredDirectory.contains(defaultDirectory),
+      ),
     );
   }
 
@@ -49,12 +43,14 @@ class RustSettingsRepositoryImpl implements SettingsRepository {
     String path,
   ) async {
     final defaultDirectory = (await appFlowyApplicationDataDirectory()).path;
-    await getIt<ApplicationDataStorage>().setCustomPath(path);
+    final storage = getIt<ApplicationDataStorage>();
+    await storage.setCustomPath(path);
+    final configuredDirectory = await storage.getPath();
 
     return FlowyResult.success(
       UserDataLocation(
-        path: path,
-        isCustom: path.contains(defaultDirectory),
+        path: configuredDirectory,
+        isCustom: !configuredDirectory.contains(defaultDirectory),
       ),
     );
   }
