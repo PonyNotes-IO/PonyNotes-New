62284d3eab9cf1a0a74956c5035f488007c52abe
Megumi1773 <lixiaojie0514@foxmail.com>
2026-08-26 02:50:12 -0700

    fix: 修复移动端协作区看板顶部分享按钮

--- frontend/appflowy_flutter/lib/mobile/presentation/base/mobile_view_page.dart
+++ frontend/appflowy_flutter/lib/mobile/presentation/base/mobile_view_page.dart
@@ -19,7 +19,6 @@ import 'package:appflowy/plugins/document/presentation/document_collaborators.da
 import 'package:appflowy/plugins/document/presentation/editor_notification.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/header/emoji_icon_widget.dart';
 import 'package:appflowy/plugins/shared/share/share_button.dart';
-import 'package:appflowy/plugins/whiteboard/application/whiteboard_space_util.dart';
 import 'package:appflowy/shared/feature_flags.dart';
 import 'package:appflowy/shared/icon_emoji_picker/flowy_icon_emoji_picker.dart';
 import 'package:appflowy/shared/icon_emoji_picker/tab.dart';
@@ -273,12 +272,9 @@ class _MobileViewPageState extends State<MobileViewPage> {
         key: ValueKey('favorite_button_${view.id}'),
         view: view,
       ),
-      if (view.layout == ViewLayoutPB.Whiteboard)
-        _MobileWhiteboardShareButton(
-          key: ValueKey('whiteboard_share_${view.id}'),
-          view: view,
-        )
-      else if (view.layout != ViewLayoutPB.Grid)
+      if (view.layout != ViewLayoutPB.Whiteboard &&
+          view.layout != ViewLayoutPB.Grid &&
+          view.layout != ViewLayoutPB.Board)
         ShareButton(
           key: ValueKey('share_button_${view.id}'),
           view: view,
@@ -330,57 +326,6 @@ class _MobileViewPageState extends State<MobileViewPage> {
           title: LocaleKeys.error_weAreSorry.tr(),
           description: LocaleKeys.error_loadingViewError.tr(),
           errorMsg: error.toString(),
-        );
-      },
-    );
-  }
-}
-
-/// 移动端私有白板不支持分享，协作白板仍保留原有分享入口。
-class _MobileWhiteboardShareButton extends StatefulWidget {
-  const _MobileWhiteboardShareButton({
-    super.key,
-    required this.view,
-  });
-
-  final ViewPB view;
-
-  @override
-  State<_MobileWhiteboardShareButton> createState() =>
-      _MobileWhiteboardShareButtonState();
-}
-
-class _MobileWhiteboardShareButtonState
-    extends State<_MobileWhiteboardShareButton> {
-  late Future<bool> _isPrivateSpace;
-
-  @override
-  void initState() {
-    super.initState();
-    _isPrivateSpace = isViewInPrivateSpace(widget.view);
-  }
-
-  @override
-  void didUpdateWidget(covariant _MobileWhiteboardShareButton oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.view.id != widget.view.id) {
-      _isPrivateSpace = isViewInPrivateSpace(widget.view);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<bool>(
-      future: _isPrivateSpace,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState != ConnectionState.done ||
-            snapshot.data == true) {
-          return const SizedBox.shrink();
-        }
-
-        return ShareButton(
-          key: ValueKey('share_button_${widget.view.id}'),
-          view: widget.view,
         );
       },
     );
